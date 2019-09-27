4b32e6b68dada9e34a24837ad48b0cf3b2f7a1a6
Bogdan <42070578+SoaQa@users.noreply.github.com>
2019-09-27 14:53:40 +0300

    Add files via upload

--- 6_backpack.py
+++ 6_backpack.py
@@ -1,3 +1,37 @@
+some_goods = {"фотоаппарат": {"weight": 1, "cost": 100}, "Мешок угля": {"weight": 20, "cost": 10}, \
+				"Ноутбук": {"weight": 3, "cost": 200}, "Телефон": {"weight": 0.3, "cost": 150}, \
+				"Тостер": {"weight": 5, "cost": 20}, "Кроссовки": {"weight": 2, "cost": 30}}
+
+
+#Метод динамического программирования
+
+def backpack(goods, max_weight):
+	t = {0:0}
+	sol = {0:[]}
+	for i in goods:
+		t_old = dict(t)
+		for x in t_old:
+			if x + goods[i]["weight"] <= max_weight:
+				if (x + goods[i]["weight"] in t) or ((x + goods[i]["weight"]) < (t_old[x] + goods[i]["cost"])):
+					t[x + goods[i]["weight"]] = (t_old[x] + goods[i]["cost"])
+					sol[x + goods[i]["weight"]] = sol[x] + [i]
+	result_cost = max(t.values())
+	result_weight = None
+	for i in t:
+		if t[i] == result_cost:
+			result_weight = i
+	#print(sol)
+	#print(t)
+	result = sol[result_weight]
+	return {"Вещи в рюкзаке": result}, {"Стоимость":result_cost}, {"Вес":result_weight}
+
+
+print("Ваирант 1\n", backpack(some_goods, 10))
+
+
+#Жадный алгоритм
+
+
 class BackPack(object):
 
 	def __init__(self, goods, max_weight):
@@ -21,18 +55,13 @@ class BackPack(object):
 		return f'Вещи в рюкзаке: {self.items_inside}.\nСуммарный вес: {self.total_weight}'
 
 
-some_goods = {"фотоаппарат": {"weight": 1, "cost": 10000}, "Мешок угля": {"weight": 20, "cost": 1000}, \
-				"Ноутбук": {"weight": 3, "cost": 20000}, "Телефон": {"weight": 0.3, "cost": 15000}, \
-				"Тостер": {"weight": 5, "cost": 2000}, "Кроссовки": {"weight": 2, "cost": 3000}}
+new_backpack1 = BackPack(goods=some_goods, max_weight=10)
 
-new_backpack1 = BackPack(goods=some_goods, max_weight=22)
 
-new_backpack2 = BackPack(goods=some_goods, max_weight=10)
+print("\nВаирант 2\n", new_backpack1)
 
-new_backpack3 = BackPack(goods=some_goods, max_weight=5)
 
-print(new_backpack1)
 
-print(new_backpack2)
 
-print(new_backpack3)
+
+
